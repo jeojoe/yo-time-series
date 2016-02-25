654ee782db0fd49fbddb248a2c3bd50a68498566
Supanat Potiwarakorn <supanat.ptk@gmail.com>
2016-02-25 08:59:27 +0700

    yeah

--- src/zup_timeseries/util.js
+++ src/zup_timeseries/util.js
@@ -39,5 +39,13 @@ export const threeDimensionDTW = (seriesA, seriesB) => {
   }, 0);
 };
 
+export const getNSimilarSeries = (series, amount, database, thresholds = 20.0) => {
+  return [
+    [{x: 0, y: 0, z: 0}],
+    [{x: 0, y: 0, z: 1}],
+    [{x: 0, y: 1, z: 0}]
+  ];
+};
+
 export const extractSeriesDimension =
   (series, dimension) => series.map(e => e[dimension]);
--- test/zup_timeseries/utilTest.js
+++ test/zup_timeseries/utilTest.js
@@ -3,7 +3,7 @@
 /*eslint no-console: 0*/
 'use strict';
 
-import { DTW, threeDimensionDTW } from '../../src/zup_timeseries/util'
+import { DTW, threeDimensionDTW, getNSimilarSeries } from '../../src/zup_timeseries/util'
 
 describe('util', () => {
   describe('#DTW', () => {
@@ -29,5 +29,33 @@ describe('util', () => {
         [ { x: 1, y: 1, z: 0 }, { x: 2, y: 1, z: 2 } ]
       )).to.equal(2);
     });
+  });
+
+  describe('#getNSimilarSeries', () => {
+    const database = [
+      [{x: 0, y: 0, z: 0}],
+      [{x: 0, y: 0, z: 1}],
+      [{x: 0, y: 1, z: 0}],
+      [{x: 1, y: 3, z: 0}],
+      [{x: 32, y: 12, z: 1}],
+      [{x: 31, y: 11, z: 1}]
+    ];
+
+    it('return 3 similar string if get three string and matches more than or equal 3', () => {
+      expect(getNSimilarSeries([{x: 2, y: 0, z: 0}]), 3 ,database)
+        .to.deep.equal([
+          [{x: 0, y: 0, z: 0}],
+          [{x: 0, y: 0, z: 1}],
+          [{x: 0, y: 1, z: 0}]
+        ]);
+    });
+
+    //it('return 2 similar string ig get 3 string and matches are just 2', () => {
+    //  expect(getNSimilarSeries({x: 32, y: 12, z: 0}), 3 ,database)
+    //    .to.deep.equal([
+    //      [{x: 32, y: 12, z: 1}],
+    //      [{x: 31, y: 11, z: 1}]
+    //    ]);
+    //});
   });
 });
